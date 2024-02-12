82bd96a1ab1a59d12ab770de01a9a2f46775be27
Rigo Villalta <40904876+Rigo-Villalta@users.noreply.github.com>
2024-02-11 18:38:27 -0600

    inicio de repositorio

---
+++ algoritmo.py
@@ -0,0 +1,6 @@
+# Algoritmo del método del descenso del gradiente
+# mejorado para soportar un función cuando esta 
+# tiende al infinito, particularmente útil para nuestra función
+
+def descenso_de_gradiente(x0, alpha, funcion):
+    pass
